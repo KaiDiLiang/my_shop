4eaca65dacd11c71b961aad605ecc3d30716b8f2
KaiDiL <kai930322@outlook.com>
2018-11-24 13:56:34 +0800

    测试往数据库添加查询数据

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -1,10 +1,19 @@
 <?php
 namespace app\admin\controller;
 
+use think\Db;
+
 class Test {
     public function index() {
-      $test = '11111';
-      return $test;
-      var_dump($test);
+      $datas = ['username' => 'kai', 'password' => md5('kai'), 'email' => 'kai930322@outlook.com'];
+      $sql = Db::table('shop_admin')->where('id', 1)->update(['password' => md5('admin')]);
+      // foreach ($datas as $key => $value) {
+      //   echo $key . ":" . $value . "</br>";
+      // }
+      // $sql = Db::table('shop_admin')->insert($datas);
+      if ($sql !== '') {
+        echo "添加成功";
+      }
+      var_dump($sql);
     }
 }
